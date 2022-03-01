60270ede426462faa84a87c5c1bb8d7a1cc459a3
Haven Ross <havenr@live.unc.edu>
2022-02-28 19:00:14 -0500

    changes 2/28

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -2,3 +2,43 @@
 
 __author__ = "730309291"
 
+
+def invert(x: dict[str, str]) -> dict[str, str]:
+    """Inverts key-value pairs of given dictionary."""
+    y: dict[str, str] = dict() 
+    for key in x:
+        value: str = x[key]
+        if value in y:
+            raise KeyError("Cannot use this key!")
+        else:
+            y[value] = key
+    return y
+
+
+def favorite_colors(a: dict[str, str]) -> str:
+    """Counts and returns most frequently mentioned color."""
+    b: dict[str, int] = {}
+    for key in a:
+        color = a[key]
+        if color in b:
+            b[color] += 1
+        else:
+            b[color] = 1
+    most_freq: str = ""
+    h: int = 0 
+    for color in b:
+        if b[color] > h:
+            h = b[color]
+            most_freq = color
+    return most_freq
+
+
+def count(my_list: list[str]) -> dict[str, int]:
+    """Counts number of mentions of certain strings in a list."""
+    result: dict[str, int] = {}
+    for item in my_list:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+    return result
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -2,3 +2,23 @@
 
 __author__ = "730309291"
 
+from dictionary import invert, favorite_colors, count
+
+
+def test_invert() -> None:
+    """Tests invert function to see if list is inverted."""
+    x: dict[str, str] = {"apple": "cat"}
+    assert invert(x) == {"cat": "apple"}
+
+
+def test_favorite_colors() -> None:
+    """Tests favorite colors function to see if mentioning most frequently listed color."""
+    a: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_colors(a) == "blue"
+
+
+def test_count() -> None:
+    """Tests count function to ensure counting strings properly."""
+    my_list: list[str] = ["apple", "carrot"]
+    result: dict[str, int]
+    assert count(my_list) == {"apple": 1, "carrot": 1}
